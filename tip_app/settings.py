1a740854e5c54186cc02b1532ac50e29a8fb7176
Constantin Kurz <constantin.kurz@ing.com>
2022-02-11 14:55:05 +0100

    changed mail server port

--- tip_app/settings.py
+++ tip_app/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shortytipp@gmail.com'
 EMAIL_HOST_PASSWORD = 'MailandMadrid'
 

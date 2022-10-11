9d632c7916188422c2575572014c51173815a071
Constantin Kurz <constantin.kurz@ing.com>
2022-10-11 07:50:04 +0200

    revise width of col_user and champion choices

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from tip_app_main.models import Team, Tip
 
 class Profile(models.Model):
 
+    champion_choices = []
     champion_choices = [(team.team_ccode, team.team_name) for team in Team.objects.all()]
     # champion_choices = [('GER', 'Deutschland'), ('FRA', 'Frankreich')]
     champion_choices.append([('---'), ('---')])

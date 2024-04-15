69dde0e5612765a1c91607ec9d4bd68dc3a0ab23
em07od <constantin.kurz@ing.com>
2024-04-15 13:57:27 +0200

    em2024 renaming

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tip_app_main.models import Team, Tip
+from tip_app_main.models import Tip
 
 
 class Profile(models.Model):
